9b95deee27043d372b006e08355d1bbc5620e195
Tamila Yefimenko <tamilaedu88@gmail.com>
2025-08-22 18:52:09 +0300

    added Rewiew with styles

--- src/components/CamperRewiew/CamperRewiew.jsx
+++ src/components/CamperRewiew/CamperRewiew.jsx
@@ -1,5 +1,49 @@
+import { useOutletContext } from 'react-router-dom';
+import s from './CamperRewiew.module.css';
+import Icon from '../Icon/Icon';
+
 const CamperRewiew = () => {
-  return <p>Rewiew</p>;
+  const { currentCamper } = useOutletContext();
+
+  if (!currentCamper) return null;
+
+  const { reviews } = currentCamper;
+
+  return (
+    <div className={s.reviewsWrapper}>
+      {reviews?.length > 0 ? (
+        reviews.map((review, index) => (
+          <div key={index} className={s.reviewCard}>
+            <div className={s.nameWrapper}>
+              <div className={s.avatar}>
+                {review.reviewer_name[0].toUpperCase()}
+              </div>
+
+              <div className={s.reviewContent}>
+                <h4 className={s.reviewerName}>{review.reviewer_name}</h4>
+
+                <div className={s.stars}>
+                  {[...Array(5)].map((_, i) => (
+                    <Icon
+                      key={i}
+                      name="icon-Property-1Default-1-1"
+                      size={16}
+                      className={
+                        i < review.reviewer_rating ? s.starActive : s.star
+                      }
+                    />
+                  ))}
+                </div>
+              </div>
+            </div>
+            <p className={s.reviewText}>{review.comment}</p>
+          </div>
+        ))
+      ) : (
+        <p>No reviews yet.</p>
+      )}
+    </div>
+  );
 };
 
 export default CamperRewiew;
